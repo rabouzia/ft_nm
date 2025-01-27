eb5792070c256a85ef09d7e6fbfafbafec8e17b0
Ramzy Zoldik <rabouzia@student.42.fr>
2025-01-27 23:05:04 +0100

    added

--- src/main.c
+++ src/main.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: rzoldik <rzoldik@student.42.fr>            +#+  +:+       +#+        */
+/*   By: ramzerk <ramzerk@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/27 22:15:32 by rabouzia          #+#    #+#             */
-/*   Updated: 2025/01/27 16:16:52 by rzoldik          ###   ########.fr       */
+/*   Updated: 2025/01/27 23:00:47 by ramzerk          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,49 +31,56 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <fcntl.h>
-#include <sys/mman.h>
 #include <unistd.h>
+#include <sys/mman.h>
+#include <sys/stat.h>
+#include <elf.h>
+#include <string.h>
 
 int main(int ac, char **av) {
 	if (ac != 2)
 		return 0;
-    int fd = open(av[1], O_RDONLY);  // Open the file in read-only mode
-    if (fd == -1) {
+    
+	int fd = open(av[1], O_RDONLY);
+    if (fd == -1) 
+	{
         perror("Failed to open file");
         return 1;
     }
+	
+	struct stat st;
+	
+	if (fstat(fd, &st) < 0) 
+	{	
+		perror("fstat");
+		close(fd);
+		exit(1);
+	}
 
-    // Get the size of the file
-    off_t file_size = lseek(fd, 0, SEEK_END);
-    if (file_size == -1) {
-        perror("Failed to get file size");
-        close(fd);
-        return 1;
-    }
+	size_t file_size = st.st_size;
+	printf("%zu", file_size);
+	
+	void *file_data = mmap(NULL, file_size, PROT_READ, MAP_PRIVATE, fd, 0);
+	if (file_data == MAP_FAILED) 
+	{
+    	perror("mmap");
+    	close(fd);
+    	exit(1);
+	}
+	// int i = 0;
+	// while(file_data[i])
+		// printf("%s\n", (char *) file_data);
 
-    // Map the file into memory
-    char *mapped = mmap(NULL, file_size, PROT_READ, MAP_SHARED, fd, 0);
-    if (mapped == MAP_FAILED) {
-        perror("Failed to map file");
-        close(fd);
-        return 1;
-    }
+	Elf64_Ehdr *elf_header = (Elf64_Ehdr *)file_data;
 
-    // Print the entire content of the file byte-by-byte
-    for (off_t i = 0; i < file_size; i++) {
-        printf("%c", mapped[i]);
-    }
-    printf("\n");
-
-    // Unmap the file and close the file descriptor
-    if (munmap(mapped, file_size) == -1) {
-        perror("Failed to unmap file");
-        close(fd);
-        return 1;
-    }
-
-    close(fd);
-    return 0;
+	if (memcmp(elf_header->e_ident, ELFMAG, SELFMAG) != 0) 
+	{
+		fprintf(stderr, "Not an ELF file\n");
+		munmap(file_data, file_size);
+		close(fd);
+		exit(1);
+	}
+		
 }
 
 
