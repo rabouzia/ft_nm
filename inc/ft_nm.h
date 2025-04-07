3513f2569e30f37394f61059fb3cf3b68e261f2d
Ramzy Zoldik <rabouzia@student.42.fr>
2025-04-07 02:28:00 +0200

    cleaning🧹

--- inc/ft_nm.h
+++ inc/ft_nm.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_nm.h                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: rzoldik <rzoldik@student.42.fr>            +#+  +:+       +#+        */
+/*   By: ramzerk <ramzerk@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/29 00:29:40 by ramzerk           #+#    #+#             */
-/*   Updated: 2025/01/27 15:10:35 by rzoldik          ###   ########.fr       */
+/*   Updated: 2025/04/07 02:26:34 by ramzerk          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,4 +20,28 @@
 #include <sys/mman.h> // munmap, mmap
 #include <fcntl.h>    // open
 #include <sys/stat.h> // fstat
+// #include <elf.h>
+#include <string.h>
+#include <unistd.h>
+#define OP_ERR "Failed to open file\n"
+#define ARG_ERR "Usage ./ft_nm [-flag] <binary>\n"
+#define FSTAT_ERR "fstat error\n"
+#define MMAP_ERR "mmap error\n"
 
+
+
+// struct stat {
+//     dev_t     st_dev;     /* ID of device containing file */
+//     ino_t     st_ino;     /* inode number */
+//     mode_t    st_mode;    /* protection */
+//     nlink_t   st_nlink;   /* number of hard links */
+//     uid_t     st_uid;     /* user ID of owner */
+//     gid_t     st_gid;     /* group ID of owner */
+//     dev_t     st_rdev;    /* device ID (if special file) */
+//     off_t     st_size;    /* total size, in bytes */
+//     blksize_t st_blksize; /* blocksize for file system I/O */
+//     blkcnt_t  st_blocks;  /* number of 512B blocks allocated */
+//     time_t    st_atime;   /* time of last access */
+//     time_t    st_mtime;   /* time of last modification */
+//     time_t    st_ctime;   /* time of last status change */
+// };
--- src/main.c
+++ src/main.c
@@ -1,72 +1,25 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   main.c                                             :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: ramzerk <ramzerk@student.42.fr>            +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/11/27 22:15:32 by rabouzia          #+#    #+#             */
-/*   Updated: 2025/01/27 23:18:50 by ramzerk          ###   ########.fr       */
-/*                                                                            */
-/* ************************************************************************** */
-
 #include "ft_nm.h"
 
-// struct stat {
-//     dev_t     st_dev;     /* ID of device containing file */
-//     ino_t     st_ino;     /* inode number */
-//     mode_t    st_mode;    /* protection */
-//     nlink_t   st_nlink;   /* number of hard links */
-//     uid_t     st_uid;     /* user ID of owner */
-//     gid_t     st_gid;     /* group ID of owner */
-//     dev_t     st_rdev;    /* device ID (if special file) */
-//     off_t     st_size;    /* total size, in bytes */
-//     blksize_t st_blksize; /* blocksize for file system I/O */
-//     blkcnt_t  st_blocks;  /* number of 512B blocks allocated */
-//     time_t    st_atime;   /* time of last access */
-//     time_t    st_mtime;   /* time of last modification */
-//     time_t    st_ctime;   /* time of last status change */
-// };
-
-#include <stdio.h>
-#include <stdlib.h>
-#include <fcntl.h>
-#include <unistd.h>
-#include <sys/mman.h>
-#include <sys/stat.h>
-#include <elf.h>
-#include <string.h>
-
 int main(int ac, char **av) {
-	if (ac != 2)
-		return 0;
-    
+	if (ac != 2 || ac != 3)
+		return(dprintf(2, ARG_ERR));
+
 	int fd = open(av[1], O_RDONLY);
     if (fd == -1) 
-	{
-        perror("Failed to open file");
-        return 1;
-    }
+		return(dprintf(2, OP_ERR));
 	
 	struct stat st;
 	
 	if (fstat(fd, &st) < 0) 
-	{	
-		perror("fstat");
-		close(fd);
-		exit(1);
-	}
+		return(close(fd), dprintf(2, FSTAT_ERR));
 
 	size_t file_size = st.st_size;
 	printf("%zu", file_size);
 	
 	void *file_data = mmap(NULL, file_size, PROT_READ, MAP_PRIVATE, fd, 0);
 	if (file_data == MAP_FAILED) 
-	{
-    	perror("mmap");
-    	close(fd);
-    	exit(1);
-	}
+		return(close(fd), dprintf(2, MMAP_ERR));
+
 	// int i = 0;
 	// while(file_data[i])
 		// printf("%s\n", (char *) file_data);
@@ -90,9 +43,3 @@ int main(int ac, char **av) {
 	}
 
 }
-
-
-/*
-	- decode les binaire (.o)
-		- utiliser mmap
-*/
